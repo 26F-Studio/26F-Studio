efdb1de5319517ba1647bb8a9c1dea8f427ec2db
ParticleG <particle_g@outlook.com>
2022-07-10 10:28:44 +0800

    - Init project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-media-player
+      type="video"
+      :sources="videoSources" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    const videoSources = [
+      {
+        src: "/videos/techmino_galaxy.webm",
+        type: "video/webm"
+      },
+      {
+        src: "/videos/techmino_galaxy.mov",
+        type: "video/quicktime"
+      }
+    ];
+    return {
+      videoSources
+    };
+  }
+});
+</script>
