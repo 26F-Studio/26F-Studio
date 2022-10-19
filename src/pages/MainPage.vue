cec60e1ad5d1376face203ebd279ca53306ee573
ParticleG <particle_g@outlook.com>
2022-10-19 23:51:19 +0800

    - Update sources

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,11 +1,11 @@
 <template>
   <q-img class="absolute-top" src="images/home-header.png" />
-  <q-page class="flex flex-center">
-    <div class="col-12 col-sm-11 col-md-10 self-start column q-gutter-y-sm">
-      <div
-        v-for="index in 1000"
-        :key="index">
-        <br />
+  <q-page class="flex flex-center column q-pa-md">
+    <div class="self-start row">
+      <div class="offset-1 col-5 header-text text-weight-bolder">
+        This is just
+        a example of
+        filler texts.
       </div>
     </div>
   </q-page>
@@ -19,3 +19,15 @@ export default defineComponent({
 
 });
 </script>
+
+<style lang="scss">
+@import "src/css/app.scss";
+
+.header-text {
+  font-family: 'newsreader-italic', serif;
+  font-style: italic;
+  font-size: 152px;
+  line-height: 152px;
+  font-feature-settings: 'pnum' on, 'lnum' on;
+}
+</style>
