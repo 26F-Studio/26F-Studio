0bf14b26972b8347a4cb25e5d9cb42e1ef285ba1
ParticleG <particle_g@outlook.com>
2022-11-10 20:27:06 +0800

    - Update sources

--- src/components/WaveCover.vue
+++ src/components/WaveCover.vue
@@ -1,12 +1,18 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" :viewbox="`0 0 ${width} ${height}`">
-    <path fill-rule="evenodd" :fill="fill"
-          :d="`M${x},${y} C${bx1},${by1} ${bx2},${by2} ${width},${y} L${width},${height} ${x},${height} ${x},${y}  Z`"/>
+    <g class="waves">
+      <path
+        v-for="(path, index) in paths"
+        :key="index"
+        fill-rule="evenodd" :fill="fill"
+        :d="path"/>
+    </g>
+
   </svg>
 </template>
 
 <script>
-import {computed, defineComponent, ref} from "vue";
+import {computed, defineComponent} from "vue";
 
 export default defineComponent({
   name: "WaveCover",
@@ -14,43 +20,79 @@ export default defineComponent({
     fill: {
       type: String,
       default: "rgba(255, 255, 255, 1)"
-    }
+    },
+    position: {
+      type: String,
+      default: "bottom"
+    },
+    waves: {
+      type: Array,
+      default: () => [{
+        period: 1,
+        amplitude: 200,
+        speed: 2
+      }]
+    },
   },
-  setup() {
-    const width = 1920, height = 1080, amplitude = 200;
-    const waveFunction = (timestamp) => {
-      return Math.sin(timestamp / 2000);
-    };
-    const differentialWaveFunction = (timestamp) => {
-      return Math.cos(timestamp / 2000);
-    };
-    const x = 0, y = ref(height - amplitude);
-    const bx1 = 700, by1 = ref(height - amplitude);
-    const bx2 = width - 700, by2 = ref(height - amplitude);
-    const animationInterval = setInterval(() => {
-      const timestamp = Date.now();
-      y.value = height - amplitude + waveFunction(timestamp) * amplitude;
-      by1.value = height - amplitude - differentialWaveFunction(timestamp) * amplitude;
-      by2.value = height - amplitude + differentialWaveFunction(timestamp) * amplitude;
-    }, 1000 / 60);
+  setup(props) {
+    const width = 1920, height = 1080;
+    const paths = computed(() => {
+      let result = [];
+      for (const wave of props.waves) {
+        const x = 0, y = height - wave.amplitude;
+        let path = `M${x},${y} `;
+        const segment = width / wave.period;
+        for (let index = 0; index < wave.period; index++) {
+          const bx1 = segment * (index + 0.33), by1 = height - wave.amplitude * (Math.sqrt(2) + 1);
+          const bx2 = width - (segment * (index + 0.33)), by2 = height + wave.amplitude * (Math.sqrt(2) - 1);
+          path += `C${bx1},${by1} ${bx2},${by2} ${segment * (index + 1)},${y} `;
+        }
+        path += `L${width},${height} ${x},${height} ${x},${y} Z`;
+        result.push(path);
+      }
+      console.log(result);
+      return result;
+    });
     return {
       width,
       height,
-      x,
-      y,
-      bx1,
-      by1,
-      bx2,
-      by2,
-      animationInterval
+      paths
     };
-  },
-  beforeUnmount() {
-    clearInterval(this.animationInterval);
   }
 });
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.waves > use {
+  animation: wave 25s cubic-bezier(.55, .5, .45, .5) infinite;
+}
 
+.waves > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+
+.waves > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+
+.waves > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+
+.waves > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+
+@keyframes wave {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(-100%, 0, 0);
+  }
+}
 </style>
