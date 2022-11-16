83327b8ff13914abdb6f6f95e031e1753473a5c5
ParticleG <particle_g@outlook.com>
2022-11-16 22:54:19 +0800

    - Update sources

--- src/components/BackgroundImage.vue
+++ src/components/BackgroundImage.vue
@@ -3,47 +3,36 @@
     :loading="eager ? 'eager' : 'lazy'"
     no-spinner
     no-transition
-    :src="require(`assets/background.png`)"
-    :srcset="srcSet"
-    :sizes="sizes"
+    :src="src"
     fit="cover">
-    <slot/>
+    <slot />
   </q-img>
 </template>
 
 <script>
-import {useQuasar} from "quasar";
-import {computed, defineComponent} from "vue";
+import { useQuasar } from "quasar";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   name: "BackgroundImage",
   props: {
     eager: {
       type: Boolean,
-      default: false,
+      default: false
     }
   },
   setup() {
     const $q = useQuasar();
-    const srcSet = computed(() => {
-      let result = "";
-      for (const [key, value] of Object.entries($q.screen.sizes)) {
-        result += `${require(`assets/background-${key}.webp`)} ${value}w,`;
+    const src = computed(() => {
+      for (const key of Object.keys($q.screen.sizes)) {
+        if ($q.screen[key]) {
+          return require(`assets/background-${key}.webp`);
+        }
       }
-      result += `${require(`assets/background.webp`)} 3360w`;
-      return result;
-    });
-    const sizes = computed(() => {
-      let result = "";
-      for (const value of Object.values($q.screen.sizes)) {
-        result += `(max-width: ${value}px) ${value}px,`;
-      }
-      result += `3360w`;
-      return result;
+      return require(`assets/background-${key}.png`);
     });
     return {
-      srcSet,
-      sizes
+      src
     };
   }
 });
