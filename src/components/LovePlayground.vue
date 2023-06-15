07d7b4a9e234fbe1a6da8e591e1e1e4545456f84
ParticleG <particle_g@outlook.com>
2023-06-15 23:43:03 +0800

    - Fix space not working

--- src/components/LovePlayground.vue
+++ src/components/LovePlayground.vue
@@ -43,12 +43,6 @@ onMounted(() => {
   window.onclick = function () {
     window.focus();
   };
-  window.addEventListener("keydown", function (e) {
-    // space and arrow keys
-    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-      e.preventDefault();
-    }
-  }, false);
 
   const module = {
     arguments: ["./game.love"],
