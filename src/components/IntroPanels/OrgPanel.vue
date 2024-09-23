1088b9284810560278027b5d3674906481dda36c
ParticleG <particle_g@outlook.com>
2024-09-23 17:11:07 +0800

    - Update sources

---
+++ src/components/IntroPanels/OrgPanel.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import LogoText from 'components/LogoText.vue';
+import { useQuasar } from 'quasar';
+import { computed } from 'vue';
+
+const { screen } = useQuasar();
+
+const panelWidth = computed(() => {
+  switch (screen.name) {
+    case 'xs':
+      return '10rem';
+    case 'sm':
+      return '15rem';
+    case 'md':
+      return '20rem';
+    case 'lg':
+      return '25rem';
+    case 'xl':
+    default:
+      return '60rem';
+  }
+});
+</script>
+
+<template>
+  <q-card flat :style="{borderRadius: '2rem', width: panelWidth}">
+    <div class="absolute-top-left q-pl-md">
+      <div class="row items-center q-gutter-x-xs q-pa-sm">
+        <LogoText :rem="3.5" />
+        <div class="text-font-redhat-bolder" style="font-size: 2.3rem">
+          <!--          26F Studio-->
+          {{ screen.name }}
+        </div>
+      </div>
+    </div>
+    <div class="absolute-bottom-right q-pa-sm">
+      <q-icon name="r_arrow_outward" size="5rem" style="font-weight: bolder" />
+    </div>
+    <img
+      alt="?playOpenly.svg?"
+      src="~assets/images/playOpenly.svg"
+    />
+  </q-card>
+</template>
+
+<style scoped lang="scss"></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,33 +1,13 @@
 <script setup lang="ts">
 import LogoText from 'components/LogoText.vue';
+import OrgPanel from 'components/IntroPanels/OrgPanel.vue';
 </script>
 
 <template>
-  <q-page class="flex row justify-center q-pa-md">
-    <div class="col-grow column" style="max-width: 1440px">
+  <q-page class="flex flex-center justify-center q-pa-md">
+    <div class="column">
       <div class="row q-gutter-x-lg">
-        <q-card
-          class="bg-primary col-5 q-pa-lg"
-          flat
-          style="border-radius: 2rem"
-        >
-          <div class="absolute-top-left q-pl-md">
-            <div class="row items-center q-gutter-x-xs q-pa-sm">
-              <LogoText :rem="3.5" />
-              <div class="text-font-redhat-bolder" style="font-size: 2.3rem">
-                26F Studio
-              </div>
-            </div>
-          </div>
-          <div class="absolute-bottom-right q-pa-sm">
-            <q-icon
-              name="r_arrow_outward"
-              size="5rem"
-              style="font-weight: bolder"
-            />
-          </div>
-          <img alt="?playOpenly.svg?" src="~assets/images/playOpenly.svg" />
-        </q-card>
+        <OrgPanel class="bg-primary col-5 q-pa-lg" />
         <div class="col-grow column q-gutter-y-lg">
           <div class="col-grow row q-gutter-x-lg">
             <q-card
@@ -47,13 +27,17 @@ import LogoText from 'components/LogoText.vue';
             >
               <div class="absolute-bottom-left q-pl-sm">
                 <div class="column">
-                  <div class="text-font-redhat-bolder q-pa-sm" style="font-size: 2.2rem;line-height: 1rem">
+                  <div
+                    class="text-font-redhat-bolder q-pa-sm"
+                    style="font-size: 2.2rem; line-height: 1rem"
+                  >
                     Check out our
                   </div>
-                  <div class="row">
-
-                  </div>
-                  <div class="text-font-redhat-bolder q-pa-sm" style="font-size: 2.2rem;line-height: 3rem">
+                  <div class="row"></div>
+                  <div
+                    class="text-font-redhat-bolder q-pa-sm"
+                    style="font-size: 2.2rem; line-height: 3rem"
+                  >
                     GitHub repos
                   </div>
                 </div>
